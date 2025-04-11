9cf530e3617491e7a4061604b247b1c9cfba069a
AnaSofia <anasofia@MacBook-Pro-de-AnaSofia.local>
2025-04-11 16:21:38 -0600

    crearREADME

---
+++ Proyecto2/crear-readmes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for dir in */ ; do
+  if [ ! -f "$dir/README.md" ]; then
+    echo "# ${dir%/}" > "$dir/README.md"
+    echo "Sección del proyecto correspondiente a la carpeta ${dir%/}." >> "$dir/README.md"
+    echo "README.md creado en $dir"
+  fi
+done
